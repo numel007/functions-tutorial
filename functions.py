a4fed9e9c5da0a48fa34ccd687d211cee31aa0e8
Jackie <taekwondo.chan@gmail.com>
2020-09-01 17:09:46 -0700

    Add functions.py

---
+++ functions.py
@@ -0,0 +1,38 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+result2 = area(6, 4)
+result3 = area(8, 9)
+
+print(result)
+print(result2)
+print(result3)
+print("\n")
+
+
+def subtract(num1, num2):
+    subtract_result = num1 - num2
+    return subtract_result
+
+subtract_result_1 = subtract(20, 5)
+subtract_result_2 = subtract(15, 10)
+
+print(subtract_result_1)
+print(subtract_result_2)
+print("\n")
+
+def divide(num3, num4):
+    divide_result = num3 / num4
+    return divide_result
+
+def add(num5, num6):
+    add_result = num5 + num6
+    return add_result
+
+add_result_1 = add(132,8)
+add_result_2 = add(468,32)
+
+print(add_result_1)
+print(add_result_2)
